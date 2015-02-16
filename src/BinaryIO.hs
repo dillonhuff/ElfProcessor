3358a61b5c692e530b42a2ed0d7ee7fdebab30e5
Dillon Huff <dillonhuff@gmail.com>
2015-02-15 20:53:45 -0600

    binary IO now works

--- src/BinaryIO.hs
+++ src/BinaryIO.hs
@@ -1,12 +1,21 @@
 
-module BinaryIO(binaryFileContents) where
+module BinaryIO(readBinaryFile) where
 
 import Data.ByteString.Char8 as Ch
 import System.IO as IO
+import System.IO.Strict as SIO
 
-binaryFileContents :: FilePath -> IO ByteString
-binaryFileContents path = do
+readBinaryFile :: FilePath -> IO ByteString
+readBinaryFile path = do
   handle <- openFile path ReadMode
   hSetBinaryMode handle True
-  contents <- IO.hGetContents handle
+  contents <- SIO.hGetContents handle
+  hClose handle
   return $ Ch.pack contents
+  
+writeBinaryFile :: ByteString -> FilePath -> IO ()
+writeBinaryFile contents path = do
+  handle <- openFile path WriteMode
+  hSetBinaryMode handle True
+  Ch.hPutStr handle contents
+  hClose handle
